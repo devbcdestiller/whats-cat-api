02f21ae87d77010d32cf6562002266fb3b46fb9b
devbcdestiller <dev.bcdestiller@gmail.com>
2022-04-03 00:54:51 +0800

    enhancement: deploy with tensorflow-serving using docker-compose

---
+++ flask_server/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+from classes import BREEDS
+from PIL import Image
+from io import BytesIO
+import numpy as np
+import requests
+import base64
+import json
+
+
+# URI which points to the tensorflow-serving model
+MODEL_URI = 'http://whatscat-serving:8501/v1/models/whatscat-serving:predict'
+app = Flask(__name__)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    input_img = request.files['img']
+    accepted_img_format = ['image/jpeg', 'image/png']
+
+    # check if input is an image
+    if input_img.content_type not in accepted_img_format:
+        return jsonify(message='File format not supported. Use JPEG or PNG.'), 406
+
+    img = Image.open(input_img)
+
+    # encode image into base 64 since tf serving only accepts base 64 encoded images
+    with BytesIO() as buffer:
+        img.save(buffer, format='JPEG')
+        b64_img = base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+    instance = [{"b64": b64_img}]
+    data = json.dumps({"instances": instance})
+    response = requests.post(MODEL_URI, data=data)
+    result = json.loads(response.text)
+    predictions = result['predictions'][0]
+    breed = BREEDS[np.argmax(predictions)]
+
+    return jsonify(message="Prediction Success", prediction=breed, data=predictions)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
